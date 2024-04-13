f81a4b4ed640532b2ea8e06a6601ce185569221a
priyaviswa13 <166889783+priyaviswa13@users.noreply.github.com>
2024-04-13 09:30:23 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,10 @@
-module fs(a,b,bin,d,bout);
-input a,b,bin;
-output d,bout;
-
-
-
+module fs(a,b,c,diff,borrow);
+input a,b,c;
+output diff,borrow;
+  wire w1,w2,w3;
+  xor g1(w1,a,b);
+  and g2(w2,~a,b);
+  xor g3(diff,w1,c);
+  and g4(w3,c,~w1);
+  or g5(borrow,w3,w2);
 endmodule
