4942135fe2f492e17c56b404426e9bb940ddcf83
priyaviswa13 <166889783+priyaviswa13@users.noreply.github.com>
2024-04-13 09:08:02 +0530

    Update Exp1_Logicgates.v

--- Exp1_Logicgates.v
+++ Exp1_Logicgates.v
@@ -1,11 +1,11 @@
-module logicgates(a,b,andgate,orgate,xorgate,nandgate,norgate,xnorgate,notgate);
+module logicgates(a,b,c1,c2,c3,c4,c5,c6,c7);
 input a,b;
-output andgate,orgate,xorgate,nandgate,norgate,xnorgate,notgate;
-and(andgate,a,b);
-or(orgate,a,b);
-xor(xorgate,a,b);
-nand(nandgate,a,b);  
-nor(norgate,a,b);
-xnor(xnorgate,a,b);
-not(notgate,a);
+output c1,c2,c3,c4,c5,c6,c7;
+and g1(c1,a,b);
+or g2(c2,a,b);
+nand g3(c3,a,b);
+nor g4(c4,a,b);
+xor g5(c5,a,b);
+xnor g6(c6,a,b);
+not g7(c7,a);
 endmodule
