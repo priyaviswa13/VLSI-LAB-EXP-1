58f4e632de45ece4664ec625b203eaf5543f5fcb
priyaviswa13 <166889783+priyaviswa13@users.noreply.github.com>
2024-04-13 09:24:46 +0530

    Update Exp1_Halfsubractor.v

--- Exp1_Halfsubractor.v
+++ Exp1_Halfsubractor.v
@@ -1,7 +1,8 @@
-module hs(a,b,difference,borrow);
-input a,b;
-output difference,borrow;
-
-
-
-endmodule
+module halfsub(diff,borrow,a,b);
+ input a,b;
+ output diff,borrow;
+ wire w1;
+ xor g1(diff,a,b);
+ not g2(w1,a);
+ and g3(borrow,w1,b);
+ endmodule
